57c28c925533c42821fd7b75bf35a5e3aa597706
ZhuJiaqi <zhujiaqi9905@gmail.com>
2024-05-28 16:40:35 +0800

    modify scripts

---
+++ scripts/clear_dataset.sh
@@ -0,0 +1,4 @@
+rm ./dataset/gpt-dataset-simplewiki/my-gpt2_text_document_valid_indexmap_*
+rm ./dataset/gpt-dataset-simplewiki/my-gpt2_text_document_train_indexmap_*
+rm ./dataset/gpt-dataset-simplewiki/my-gpt2_text_document_test_indexmap_*
+rm -rf my-gpt2_text_document/
