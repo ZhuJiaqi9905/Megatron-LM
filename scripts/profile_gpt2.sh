0374db99f93a42d3665ca908de528a034d5e6bd4
ZhuJiaqi <zhujiaqi9905@gmail.com>
2024-06-17 17:20:04 +0800

    try to run profile and autoconfig

--- scripts/profile_gpt2.sh
+++ scripts/profile_gpt2.sh
@@ -1,12 +1,12 @@
-
-DATA_PATH=/workspace/python/Megatron-LM/dataset/gpt-dataset-simplewiki/my-gpt2_text_document
-VOCAB_FILE=/workspace/python/Megatron-LM/dataset/gpt2-vocab.json
-MERGE_FILE=/workspace/python/Megatron-LM/dataset/gpt2-merges.txt
+DATA_PATH=/mnt/gpu-91/dataset/gpt-dataset-simplewiki/my-gpt2_text_document
+VOCAB_FILE=/mnt/gpu-91/dataset/gpt2-vocab.json
+MERGE_FILE=/mnt/gpu-91/dataset/gpt2-merges.txt
+CHECKPOINT_PATH=/mnt/gpu-91/varuna/checkpoints/${model}
 GPUS_PER_SERVER=4
 
 # NCCL_SOCKET_IFNAME=enp NCCL_DEBUG=INFO GLOO_SOCKET_IFNAME=enp216s0np0,enp94s0np0
 
-python -m varuna.run_varuna --batch_size 8192 \
+python -m varuna.run_varuna  --nstages 4 --chunk_size 1 --batch_size 8192 \
         --manager_ip 172.21.0.91 \
         --gpus_per_node $GPUS_PER_SERVER --no_morphing pretrain_gpt2.py \
         --num-layers 24 \
