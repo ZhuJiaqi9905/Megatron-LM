682debca7ff346e36bd851b906ed0c50310b3504
ZhuJiaqi <zhujiaqi9905@gmail.com>
2024-10-18 10:42:47 +0800

    add 6.7B

---
+++ examples/pretrain_gpt_6_7B.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# Runs the "345M" parameter model
+
+export CUDA_DEVICE_MAX_CONNECTIONS=1
+
+GPUS_PER_NODE=4
+# Change for multinode config
+MASTER_ADDR=172.21.0.91
+MASTER_PORT=6000
+NNODES=4
+NODE_RANK=$1
+WORLD_SIZE=$(($GPUS_PER_NODE*$NNODES))
+
+VOCAB_FILE=/workspace/file/vocabs/gpt2-vocab.json
+MERGE_FILE=/workspace/file/vocabs/gpt2-merges.txt
+
+DISTRIBUTED_ARGS="
+    --nproc_per_node $GPUS_PER_NODE \
+    --nnodes $NNODES \
+    --node_rank $NODE_RANK \
+    --master_addr $MASTER_ADDR \
+    --master_port $MASTER_PORT
+"
+
+
+GPT_ARGS="
+    --tensor-model-parallel-size 1 \
+    --pipeline-model-parallel-size 4 \
+    --num-layers 32 \
+    --hidden-size 4096 \
+    --num-attention-heads 32 \
+    --seq-length 1024 \
+    --max-position-embeddings 1024 \
+    --micro-batch-size 8 \
+    --global-batch-size 2048 \
+    --lr 0.00015 \
+    --train-iters 500000 \
+    --lr-decay-iters 320000 \
+    --lr-decay-style cosine \
+    --min-lr 1.0e-5 \
+    --weight-decay 1e-2 \
+    --lr-warmup-fraction .01 \
+    --clip-grad 1.0 \
+    --tokenizer-type GPT2BPETokenizer \
+    --use-mcore-models \
+    --transformer-impl local \
+    --fp16
+"
+
+DATA_ARGS="
+    --vocab-file $VOCAB_FILE \
+    --merge-file $MERGE_FILE \
+    --mock-data \
+"
+
+OUTPUT_ARGS="
+    --log-interval 1 \
+    --timing-log-level 2 \
+    --timing-log-option all \
+    --save-interval 10000 \
+    --eval-interval 1000 \
+    --eval-iters 10 \
+"
+
+torchrun $DISTRIBUTED_ARGS pretrain_gpt.py \
+    $GPT_ARGS \
+    $DATA_ARGS \
+    $OUTPUT_ARGS \
+    --distributed-backend nccl \
